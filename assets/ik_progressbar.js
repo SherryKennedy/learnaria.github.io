457f9475f0bbb505f15306b77dd7820bb2a7bcd4
SherryKennedy <sherry_kennedy@hotmail.com>
2018-10-05 00:08:48 -0400

    Progress bar features
    Activity 7 Progress Bar: Progress bar runs saying instructions, space bar/enter, on how to progress. Announces % progress. Keyboard can be used to announce percentage. When pb (100%) complete announces Loading Complete.

--- assets/ik_progressbar.js
+++ assets/ik_progressbar.js
@@ -1,7 +1,8 @@
 ;(function ( $, window, document, undefined ) {
-	
+		//Activity 7 only chrome vox for progrssbar , create instr. how to operate bar with screen reader and keyboard
 	var pluginName = 'ik_progressbar',
-		defaults = { // values can be overitten by passing configuration options to plugin constructor 
+		defaults = { // values can be overitten by passing configuration options to plugin constructor
+			'instructions': 'Press spacebar, or Enter to get progress',
 			'max': 100
 		};
 	
@@ -27,20 +28,42 @@
 	Plugin.prototype.init = function () { // initialization function
 		
 		var id = 'pb' + $('.ik_progressbar').length;
-				
+		//Activity 7: add attributes, keyboard not initially focusable 	
 		this.element
 			.attr({
 				'id': id,
+				'tabindex': -1, // add current element to tab oder
+				'role': 'progressbar', // assign  progressbar role
+            'aria-valuenow': 0, // set current value to 0
+            'aria-valuemin': 0, // set minimum (start) value to 0 (required by screen readers)
+            'aria-valuemax': this.options.max, // set maximum (end) value
+            'aria-describedby': id + '_instructions' // add aria-describedby attribute
 			})
 			.addClass('ik_progressbar')
-      ;
+			//Activity 7 keydown add here
+			.on('keydown.ik', {'plugin': this}, this.onKeyDown);
 		
 		this.fill = $('<div/>')
 			.addClass('ik_fill');
 			
 		this.notification = $('<div/>') // add div element to be used to notify about the status of download
+		//Activity 7, add attr to div 
+		   .attr({
+            'aria-live': 'assertive', // set notofocation priority to high
+            'aria-atomic': 'additions' // notify only about newly added text
+            })
 			.addClass('ik_readersonly')
 			.appendTo(this.element);
+		
+		//Activity 7; add div element
+	$('<div/>') // add div element to be used with aria-described attribute of the progressbar
+			 .text(this.options.instructions) // get instruction text from plugin options
+					 .addClass('ik_readersonly') // hide element from visual display
+					 .attr({
+					 'id': id + '_instructions',
+					 'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
+	   })
+		 .appendTo(this.element);
 
 		$('<div/>')
 			.addClass('ik_track')
@@ -57,9 +80,10 @@
 	Plugin.prototype.getValue = function() {
 		
 		var value;
-		
-		value = Number( this.element.data('value') ); // inaccessible
-		
+		//Activity 7, replace data value which receives valuew of pb
+		//use aria-valuenow attrib with screen reader
+		//value = Number( this.element.data('value') ); // inaccessible
+		value = Number( this.element.attr('aria-valuenow') ); // accessible
 		return parseInt( value );
 		
 	};
@@ -99,11 +123,14 @@
 		}
 		
 		this.element
-			.data({ // inaccessible
+		//Activity 7, remove data value, use attr from aria-value now
+			/*.data({ // inaccessible
 				'value': parseInt(val) 
 			}) 
-      ;
-		
+      ;*/
+			.attr({ // accessible
+            'aria-valuenow': val
+        });
 		this.updateDisplay();
 		
 	};
@@ -132,6 +159,32 @@
 		this.notify();
 	
 	};
+	
+	
+	//Activity 7, plugin onkeydonw function
+	/**
+ * Handles kedown event on progressbar element.
+ *
+ * @param {Object} event - Keyboard event.
+ * @param {object} event.data - Event data.
+ * @param {object} event.data.plugin - Reference to plugin.
+ */
+Plugin.prototype.onKeyDown = function(event) {
+       
+    switch(event.keyCode) {
+           
+        case ik_utils.keys.space:
+        case ik_utils.keys.enter:
+            event.preventDefault();
+            event.stopPropagation();
+            event.data.plugin.notify();
+            break;
+    }
+ 
+       
+};
+	
+	
 	
 	$.fn[pluginName] = function ( options ) {
 		
