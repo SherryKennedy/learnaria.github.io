888b3b49ff4f1df99525f343b5ed6cf01bc7af24
SherryKennedy <sherry_kennedy@hotmail.com>
2018-10-18 23:03:14 -0400

    Activity 12: Menu and Sub Menu Udates
    Instructions announced on how to use menu bar with a keyboard when menu bar first receives focus. List item semantics replaced with menu semantics. Submenus receive focus, presence of a submenu is announced. Only elements of the menu bar that are in view are able to receive focus. Keyboard operability for a menu bar, menubar functions according to announced. Left/Right to go through main menu, Space to enter submenu, up/down to go through submenu, Esc to get out of submenu, enter activates menu item.

--- assets/ik_menu.js
+++ assets/ik_menu.js
@@ -1,7 +1,9 @@
 ;(function ( $, window, document, undefined ) {
  
 	var pluginName = 'ik_menu',
-		defaults = {};
+		defaults = {//Activity 12, add instructions for menu keyboard to default options
+      'instructions': 'Use arrow keys to navigate between menuitems, spacebar to expand submenus, escape key to close submenus, enter to activate menuitems.'
+    };
 	 
 	/**
 	 * @constructs Plugin
@@ -31,51 +33,104 @@
 			.attr({
 				'id': id
 			});
-		
+		//instruction <div>
 		$('<div/>') // add div element to be used with aria-described attribute of the menu
 			.text(plugin.options.instructions) // get instruction text from plugin options
 			.addClass('ik_readersonly') // hide element from visual display
-			.attr({
-				'id': id + '_instructions'
+			.attr({ //Activity 12, hide instructions till needed
+				'id': id + '_instructions',
+        'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
 			})
 			.appendTo(this.element);
-			
+		//Activity 12, Add role=menubar to top level <ul> in menu 
+    //make the UL keyboard focusable with tabindex=0, so it reads the instr. while in focus being ref. with aria-labelledby 	
 		$elem.find('ul:eq(0)')
 			.attr({
-				'id': id
+				'id': id,
+        'role': 'menubar', // assign menubar role to the topmost ul element
+       'tabindex': 0,
+       'aria-labelledby': id + '_instructions'
 			});
-		
+      
+    //Activity 12, all menu items in menubar have submenus 
+    //role=menu to <ul> and hide them by dflt using
+    //aria-hidden=true  
+    $elem.find('li>ul')
+        .attr({
+          'role': 'menu',
+          'aria-hidden': true // hide submenus from screen reader
+        });
+        
 		plugin.menuitems = $elem.find('li') // setup menuitems
 			.css({ 'list-style': 'none' })
 			.each(function(i, el) {
 				
 				var $me, $link;
-				
+				//Activity 12, hide the links in the menu items from reader 
+        //dftl using tabindex= -1 and setting aria-hidden=true
 				$me = $(this);
-				$link = $me.find('>a');
-				
-				$me.has('ul').addClass('expandable');
+				$link = $me.find('>a')
+        .attr({ // disable links
+          'tabindex': -1,
+          'aria-hidden': true
+        })
+        ;
+				//Activity 12, setup the menu items throughout the menu using role=menuitem. Remove keyboard access by dflt. using tabindex=-1
+        //Label each menu itme with text of the associated link using aria-label="[$link.text]"
+        $me.attr({
+          'role': 'menuitem', // assign menuitem rols
+          'tabindex': -1,  // remove from tab order
+          'aria-label': $link.text() // label with link text
+          });
+        //Activity 12, for each menu items that has a submenu 
+        //add aria-haspopup=true to announce the presence of the submenu, and set its dflt state to collapsed 
+        //by adding aria-expanded=false
+				$me.has('ul')
+        .attr({ // setup submenus
+          'aria-haspopup': true,
+          'aria-expanded': false
+        })
+        .addClass('expandable');
 			});
 		
 		plugin.selected = plugin.menuitems // setup selected menuitem
-			.find('.selected');
+			.find('.selected')
+      //Activity 12 when menu item is marked slected, also 
+      //add aira-selected=true and keyboard access back to the menuitem with tabindex= 0 
+      .attr({
+        'tabindex': 0,
+        'aria-selected': true
+      })
+      ;
 		
 		if (!plugin.selected.length) {
-			
+			//Acitivity 12, addkeyboard access back to menu itmes 
+      //using tabindex=0, 2 cases
 			plugin.menuitems
-				.eq(0);
+				.eq(0)
+        .attr({
+            'tabindex': 0
+         })
+        ;
 			
 		} else {
 			
 			plugin.selected
-				.parentsUntil('nav', 'li');
+				.parentsUntil('nav', 'li')
+        .attr({
+          'tabindex': 0
+        })
+        ;
 			
 		}
-		
+		//Acityivty 12, add keyboard access where mouse events are 
+    //defined in the onKeyDown function
 		plugin.menuitems // setup event handlers
 			.on('mouseenter', plugin.showSubmenu)
 			.on('mouseleave', plugin.hideSubmenu)
-			.on('click', {'plugin': plugin}, plugin.activateMenuItem);
+			.on('click', {'plugin': plugin}, plugin.activateMenuItem)
+      .on("keydown", {'plugin': plugin}, plugin.onKeyDown)
+      ;
 			
 		$(window).on('resize', function(){ plugin.collapseAll(plugin); } ); // collapse all submenues when window is resized
 		
@@ -94,7 +149,20 @@
 		$submenu = $elem.children('ul');
 		
 		if ($submenu.length) {
-			$elem.addClass('expanded');
+			$elem.addClass('expanded')
+      //Activity 12, add aria-expanded=true to submenus 
+      //when they are expanded, remove keyboard access 
+      //from the submenu container with tabindex=-1.
+      //make submenu visible with aria-hidden = false
+        .attr({
+            'aria-expanded': true,
+            'tabindex': -1
+        })
+      ;
+      $submenu
+       .attr({
+           'aria-hidden': false
+       });
 		}
 	};
 	
@@ -111,7 +179,15 @@
 		$submenu = $elem.children('ul');
 		
 		if ($submenu.length) {
-			$elem.removeClass('expanded');
+			$elem.removeClass('expanded')
+      //Activity 12, set aria-expanded=false, 
+      .attr({'aria-expanded': false})
+      ;
+      //Acivity 12, hide submenus 
+      //with aria-hidden = true, remove keyboard access with 
+      //tabindex = -1
+      $submenu.attr({'aria-hidden': true});
+      $submenu.children('li').attr({'tabindex': -1});
 
 		}
 	}
@@ -125,7 +201,16 @@
 	Plugin.prototype.collapseAll = function(plugin, $elem) {
 		
 		$elem = $elem || plugin.element;
-		
+		//Activity 12, to callapse menus, reverse all attributes 
+    //defining the element as ope, reversing:
+    //aria-hidden=true, aria-expanded=false, tabindex=0 to be able open again
+    $elem.find('[aria-hidden=false]')
+      .attr({'aria-hidden': true});
+    $elem.find('.expanded')
+      .removeClass('expanded')
+      .attr({'aria-expanded': false});
+    $elem.find('li')
+      .attr({'tabindex':-1}).eq(0).attr({'tabindex': 0});
 	};
 	
 	/** 
@@ -152,7 +237,184 @@
 		
 	};
 	
-	
+	//Activity 12, keyboard accessibility:
+  //implement a subset of the keyboard interaction W3C recommends an onKeyDown function that is called when event
+  //handlers are set up for menu items. These keys include:
+  //Left/Right Arrows, Up/Down Arrows, space bar and Enter key
+  //Tab and Esc keys. 
+  /**
+  * Selects specified tab.
+  *
+  * @param {Object} event - Keyboard event.
+  * @param {object} event.data - Event data.
+  * @param {object} event.data.plugin - Reference to plugin.
+  * @param {object} event.data.index - Index of a tab to be selected.
+  */
+  
+  
+ Plugin.prototype.onKeyDown = function (event) {
+    
+     var plugin, $elem, $current, $next, $parent, $submenu, $selected;
+    
+     plugin = event.data.plugin;
+     $elem = $(plugin.element);
+     $current = $(plugin.element).find(':focus');
+     $submenu = $current.children('ul');
+     $parentmenu = $($current.parent('ul'));
+     $parentitem = $parentmenu.parent('li');
+        
+     switch (event.keyCode) {
+        
+         case ik_utils.keys.right:
+          //  console.log("at key right")
+            
+             event.preventDefault();
+             //console.log("current parent")
+            // console.log($current.parents('ul'))
+             //console.log("next li should have tabindex 0 and focus")
+             //console.log("at right arrow key, length of submenu")
+             //console.log($submenu.length)
+             //originally
+             if ($current.parents('ul').length == 1) {
+               $current.attr({'tabindex': -1}).next('li').attr({'tabindex': 0}).focus();
+             }
+             
+             //go to the right testing through submenu to menu
+            /*  if ($current.parents('ul').length == 1) {//on main header go to right
+                // $parentitem.addClass('expanded'); 
+                  $current.attr({'tabindex': -1}).next('li').attr({'tabindex': 0}).focus();
+              } else if ($submenu.length == 1){//want submenu to go over to right when not on parent
+                //  $parentitem.addClass('expanded'); 
+                  $submenu.attr({'aria-hidden': 'false'}).show();
+                  $submenu.find('li:eq(0)').attr({"tabindex": 0}).focus();
+              }*/
+             
+             break;
+        
+         case ik_utils.keys.left:
+            console.log("at key left")
+             event.preventDefault();
+            
+             if ($current.parents('ul').length == 1) {
+                 $current.attr({'tabindex': -1}).prev('li').attr({'tabindex': 0}).focus();
+             }
+            
+             break;
+            
+         case ik_utils.keys.up:
+         //Activity 12, key up used to move into and out of a submenu, and to move between menu items in a submenu.
+         //Instructions say to only use to move up for submenus. (use esc to get out of menu)
+            console.log("at key up")
+             event.preventDefault();
+             event.stopPropagation();
+             console.log("at key up, parent length:")
+             console.log($current.parents('ul').length)
+             
+             
+             
+             $current.attr({'tabindex': -1}).prev('li').attr({'tabindex': 0}).focus();
+             //up arrow will collapse submenu on 
+            // if ($parentmenu.parent().attr('aria-haspopup')) {
+                //  // $parentitem.removeClass('expanded');
+				        //  // $current.attr({"tabindex": -1});
+				        //  // $parentmenu.attr({'aria-hidden': 'true'}).hide().parent().focus();
+                //  $current.attr({'tabindex': -1}).prev('li').attr({'tabindex': 0}).focus();
+			       //} 
+            // else if ($current.parents('ul').length > 1) { 
+                //  //regular link on to go up one, ??????????????????????????????????????????????
+                  //$current.attr({'tabindex': -1}).prev('li').attr({'tabindex': 0}).focus();
+             //}
+             break;
+        
+         case ik_utils.keys.down:
+         //Activity 12, key down: opens menu and moves focus to first menuitem
+         //down arrow: moves focus to next menu item, if focus 
+         // on the last menu itme, moves focus to first (optional)
+            // console.log("at key down")
+             event.preventDefault();
+             event.stopPropagation();
+             //console.log(plugin.options.autoCollapse)
+             //console.log("current parent")
+             //console.log($current.parents('ul'))
+             //console.log("next li should have tabindex 0 and focus")
+             //console.log("length of ul")
+             //var ulLength = $current.parents('ul').length
+             //console.log(ulLength)
+             
+             //for links going down (working with down arrow from menu as well
+          /*   if ($current.parents('ul').length > 1) {
+                     $current.attr({'tabindex': -1}).next('li').attr({'tabindex': 0}).focus();
+             } else if ($submenu.length ==1) {
+                     $submenu.attr({'aria-hidden': 'false'}).show();
+             }*/
+            //originally
+             if($current.parents('ul').length > 1) {
+              $current.attr({'tabindex': -1}).next('li').attr({'tabindex': 0}).focus();
+             }
+            
+            
+             break;
+            
+         case ik_utils.keys.space:
+            
+             event.preventDefault();
+             event.stopPropagation();
+            //Activity 12, found that this only works the first time and not subsequent, to open the submenu
+            /* if($submenu.length) {
+                 plugin.showSubmenu(event);
+                 $submenu.children('li:eq(0)').attr({'tabindex': 0}).focus();
+             } */
+             //Acitivity 12, space bar used to activate a menu item
+             //plugin.activateMenuItem(event);
+             //instuctions when go on menu say it is supposed to open submenu
+             if ($submenu.length ==1) {
+                     $submenu.attr({'aria-hidden': 'false'}).show();
+                     $submenu.find('li:eq(0)').attr({"tabindex": 0}).focus();
+             }
+             break;
+        
+         case ik_utils.keys.esc:
+            //Close the current submenu
+             event.stopPropagation();
+            
+             if ($parentitem.hasClass('expandable')) {
+                
+                 $parentitem.removeClass('expanded').attr({
+                     'tabindex': 0,
+                     'aria-expanded': false
+                 }).focus();
+                 plugin.collapseAll(plugin, $parentitem); 
+             }
+             //Activity 12, close the current submenus
+             //up arrow will collapse submenu on 
+             if ($parentmenu.parent().attr('aria-haspopup')) {
+                  // $parentitem.removeClass('expanded');
+                  $current.attr({"tabindex": -1});
+                  $parentmenu.attr({'aria-hidden': 'true'}).hide().parent().focus();
+            } 
+             
+             
+             
+             break;
+        
+         case ik_utils.keys.enter:
+            
+             plugin.activateMenuItem(event);
+            
+             break;
+        
+         case ik_utils.keys.tab:
+            //Found that this does not work correct.   
+            // http://api.jqueryui.com/menu/   $( ".selector" ).menu( "collapseAll", null, true );  or $( ".selector" ).menu( "destroy" );
+             plugin.collapseAll(plugin);
+             //pulgin.collapseAll(plugin,true);
+             //plugin.menu("collapseAll",null,true);
+             //$elem.menu("destroy");
+             break;
+     }
+ }  
+  
+  
 	$.fn[pluginName] = function ( options ) {
 		
 		return this.each(function () {
