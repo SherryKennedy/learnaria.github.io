14b1d435d0d588518495d25f3a416b9c5919fbd3
SherryKennedy <sherry_kennedy@hotmail.com>
2018-10-19 00:11:00 -0400

    Activity 14 Update Sortable LIst
    Instructions are announced on using the sortable list with a keyboard when it first receives focus. When navigating through list items, their psoition is announced along with an indication they can be moved. Keyboard operation, Tab key is used to navigate into the list, and exit the list. Up/Down arrows are used to move between list items. Window Ctrl (mac Command) + Up/Down arrow key selects a list item and moves it to a new location.

--- assets/ik_sortable.js
+++ assets/ik_sortable.js
@@ -1,7 +1,14 @@
 ;(function ( $, window, document, undefined ) {
  
 var pluginName = "ik_sortable",
-	defaults = {};
+	//Activity 14, instructions for sortable list with keyboard. 
+  //Modifier for MAC Command key otherwise Ctrl keys
+  //Standard access key commands: (work as a modifier and be described)
+  // (e.g. Ctrl+Alt on Mac, or Ctrl on Windows).
+  modifier = navigator.platform.indexOf('Mac') > -1 ? 'Command' : 'Control',
+  defaults = {
+    'instructions': 'Use arrow keys to select a list item,  ' +  modifier + ' + arrow keys to move it to a new position.'
+  };
 	 
 	function Plugin( element, options ) {
 		
@@ -19,26 +26,58 @@ var pluginName = "ik_sortable",
 		
 		plugin = this;
 		id = 'sortable_' + $('.ik_sortable').length;
+    
+    //Activity 14, assign redundant role=list, to opening UL
+    //make the UL keyboard focusable, and attach the instruction 
+    //with aria-labelledby="[instruction div id]" so keyboard 
+    //navigation details are announced when the list initially 
+    //receives focus while using a screen reader. 
 		$elem = this.element.attr({
-			'id': id
-		})
+			'id': id,
+      'role': 'list',
+      'tabindex': 0,
+      'aria-labelledby': id + '_instructions'
+		  })
 		.wrap('<div class="ik_sortable"></div>').before(plugin.temp);
 			
 		total = $elem.children('li').length;
-			
+		
+    //Activity 14, generate the <div> that will contain 
+    //instr. and add aria-hidden=true to hid it by dflt from reader 
+    $('<div/>') // add div element to be used with aria-describedby attribute of the menu
+      .text(plugin.options.instructions) // get instruction text from plugin options
+      .addClass('ik_readersonly') // hide element from visual display
+      .attr({
+          'id': id + '_instructions',
+          'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
+         })     
+       .appendTo($elem);
+    	
 		plugin.items = $elem.children('li').each( function(i, el) {
 			
+      //Activity 14, draggable below, add a redundatn role=listitem
+      //generate a label for each item that describes the list item's current position and that that list item is 'movable'
+      //set tabindex=0 on the first list item, ensure a list item  
+      //is focusable by default
 			$(el).attr({
 				'draggable': true,
-				'id': id + '_' + i
+				'id': id + '_' + i,
+        'role': 'listitem',
+        'aria-label': $(el).text() + ' ' + (i + 1) + ' of ' + total + ' movable',
+        'tabindex': i > 0 ? -1 : 0
 			});
 		})
+    
+    //Activity 14, where draggable attributes are defined near 
+    //the end of the init function, attach a keydown reference to 
+    //the onKeyDown function to make the list draggable with a keyboard 
 		.on('dragstart', {'plugin': plugin}, plugin.onDragStart)
 		.on('drop', {'plugin': plugin}, plugin.onDrop)
 		.on('dragend', {'plugin': plugin}, plugin.onDragEnd)
 		.on('dragenter', {'plugin': plugin}, plugin.onDragEnter)
 		.on('dragover', {'plugin': plugin}, plugin.onDragOver)
-		.on('dragleave', {'plugin': plugin}, plugin.onDragLeave);
+		.on('dragleave', {'plugin': plugin}, plugin.onDragLeave)
+    .on('keydown', {'plugin': plugin}, plugin.onKeyDown);
 		
 		
 	};
@@ -127,9 +166,63 @@ var pluginName = "ik_sortable",
 		
 		plugin.items.each( function(i, el) {
 			var $me = $(el);
-		});
+      //Activity 14, update the label for moved items to reflect 
+      //their new position in the list using aria-label="[new posiion]"
+      $me.attr({
+          'aria-label': $me.text() + ' ' + (i + 1) + ' of ' + plugin.items.length + ' movable'
+      });
+      
+    });  //end of function plugin items each
 		
-	}
+	}  //end of resetNumbering
+  
+  //Activity 14, for the sortable list, defines up and down arrow keys, along iwth a roving tabindex. W3C has not yet created a best practice for authoring keyboard interaction for drag and drop elements
+  Plugin.prototype.onKeyDown = function (event) {
+ 
+        var plugin, $me, currIndex, nextIndex;
+       
+        plugin = event.data.plugin;
+        $me = $(event.currentTarget);
+        currentIndex = plugin.items.index(event.currentTarget);
+           
+        switch (event.keyCode) {
+           
+            case ik_utils.keys.down:
+               
+                plugin.items.attr({'tabindex': -1});
+               
+                if(currentIndex < plugin.items.length - 1) {
+                    if(event.ctrlKey || event.metaKey) { // move item down
+                        $me.insertAfter( $me.next() );
+                        $me.attr({'tabindex': 0}).focus();
+                        plugin.resetNumbering(plugin);
+                    } else { // move focus to the next item
+                        $me.next().attr({'tabindex': 0}).focus();
+                    }
+                }
+               
+                break;
+           
+            case ik_utils.keys.up:
+               
+                plugin.items.attr({'tabindex': -1});
+               
+                if(currentIndex > 0) {
+                    if(event.ctrlKey || event.metaKey) { // move item up
+                        $me.insertBefore( $me.prev() );
+                        $me.attr({'tabindex': 0}).focus();
+                        plugin.resetNumbering(plugin);
+                    } else { // move focus to the previous item
+                        $me.prev().attr({'tabindex': 0}).focus();
+                    }
+                }
+               
+                break;
+           
+        }
+       
+};
+  
 
 	$.fn[pluginName] = function ( options ) {
 		
